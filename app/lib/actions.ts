47ea2ba0d8cb39ea26c02920c62e8de92fb268cc
trandat-us <minhdattran@gmail.com>
2025-04-21 06:01:58 +0000

    implement authentication and authorization with NextAuth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -105,4 +107,22 @@ export async function deleteInvoice(id: string) {
   }
 
   revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      if (error.message === 'CredentialsSignin') {
+        return 'Invalid credentials.';
+      } else {
+        return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
